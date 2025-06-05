2c096657aeb1b3ae854c02a5279072e37d80daba
ignatius.stanislaus <stanize@gmail.com>
2025-06-05 20:07:55 +0200

    Login LoginAA

--- src/main/java/com/transact/dashboard/SecurityConfig.java
+++ src/main/java/com/transact/dashboard/SecurityConfig.java
@@ -1,3 +1,5 @@
+package com.transact.dashboard;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
