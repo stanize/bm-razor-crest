de59dc2b390e80c527106954eddc57f5e249b231
ignatius.stanislaus <stanize@gmail.com>
2025-06-06 05:30:52 +0200

    Login LoginAA

--- src/main/java/com/transact/dashboard/DashboardMainController.java
+++ src/main/java/com/transact/dashboard/DashboardMainController.java
@@ -3,45 +3,19 @@ package com.transact.dashboard;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-
-import javax.servlet.http.HttpServletRequest;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
-@RequestMapping("/transact-dashboard")
 public class DashboardMainController {
 
-    @GetMapping
-    public String home(Model model) {
-        model.addAttribute("page", "home");
-        model.addAttribute("contentFragment", "dashboard-home");
-        return "maindashboard";
+    @GetMapping("/dashboard-home")
+    public String dashboardHome() {
+        return "dashboard-home";
     }
 
-    @GetMapping("/jboss")
-    public String jboss(Model model) {
-        //String status = getJbossStatus(); // implement this
-        String status = "To be Implemented"; // implement this
-        model.addAttribute("page", "jboss");
-        model.addAttribute("jbossStatus", status);
-        model.addAttribute("contentFragment", "dashboard-jboss");
-        return "maindashboard";
-    }
-
-    @GetMapping("/tsm")
-    public String tsm(Model model) {
-        //String status = getTsmStatus(); // implement this
-        String status = "To be Implemented"; // implement this
-        model.addAttribute("page", "tsm");
-        model.addAttribute("tsmStatus", status);
-        model.addAttribute("contentFragment", "dashboard-tsm");
-        return "maindashboard";
-    }
-
-    @GetMapping("/logs")
-    public String logs(Model model) {
-        model.addAttribute("page", "logs");
-        model.addAttribute("contentFragment", "dashboard-logs");
-        return "maindashboard";
+    @GetMapping("/button-action")
+    public String handleButtonClick(@RequestParam("name") String buttonName, Model model) {
+        model.addAttribute("responseMessage", "Button clicked: " + buttonName);
+        return "dashboard-home";
     }
 }
